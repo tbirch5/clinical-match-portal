cf85f02ef936411ccc8f06205655d40faab60d2b
Tedra <tbirch9@wgu.edu>
2025-07-01 14:11:57 -0400

    Login Form component and login page built

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,61 @@
+'use client';
+import { useState } from 'react';
+import { supabase } from '@/lib/supabaseClient';
+import { useRouter } from 'next/navigation';
+import { Fascinate } from 'next/font/google';
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError('');
+
+    if (!email || !password) {
+        setError('Email and password are required.');
+    }
+
+    setLoading(true);
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    setLoading(false);
+
+    if (error) {
+      setError(error.message)
+    } else {
+      router.push('/dashboard')
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin} className="max-w-sm mx-auto mt-10 p-4 bg-white rounded shadow">
+      <h2 className="text-2xl mb-4">Login</h2>
+      {error && <p className="text-red-500">{error}</p>}
+      <input
+        type="email"
+        placeholder="Email"
+        className="border w-full mb-3 p-2"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="border w-full mb-3 p-2"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit" className="bg-blue-600 text-white px-4 py-2 w-full disabled:opacity-50" disabled={loading}>
+        {loading ? 'Loggin in...' : 'Sign In'}
+      </button>
+    </form>
+  );
+}
